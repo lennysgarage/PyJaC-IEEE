37741a7b8788387d088958438dd2a5dd5b7824c1
lennysgarage <lennysgarage1@gmail.com>
2021-01-14 16:18:52 -0500

    Added the ability to convert from IEEE-754 single precison to a base 10 decimal number.

---
+++ src/sample/IEEEToDec.java
@@ -0,0 +1,44 @@
+package sample;
+
+public class IEEEToDec {
+
+    private String binString;
+
+    public IEEEToDec(String binString) {
+        this.binString = binString;
+    }
+
+    public void getDec() {
+        System.out.println(this.calcIEEEToDec());
+    }
+
+    private int getSignBit(){
+        return this.binString.charAt(0);
+    }
+
+    private double getSummation(){
+        double sum = 0;
+        char[] mantissa = this.binString.substring(9).toCharArray();
+        for(int i = 0; i < 23; i++){
+            sum += Character.getNumericValue(mantissa[i]) * Math.pow(2,-(i+1));
+        }
+        return 1 + sum;
+    }
+
+    private int getExponent(){
+        int exponent = 0;
+        char[] exponentBits = this.binString.substring(1,9).toCharArray();
+        for(int i = 0; i <= 7; i++){
+            exponent += Character.getNumericValue(exponentBits[i]) * Math.pow(2,7-i);
+        }
+        return exponent;
+    }
+
+    private double calcIEEEToDec(){
+        // Special case for when the value is 0.
+        if (this.getExponent() == 0){
+            return 0;
+        }
+        return Math.pow(-1, this.getSignBit()) * this.getSummation() * Math.pow(2, this.getExponent() - 127);
+    }
+}
