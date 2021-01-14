886024138720dda060d20db5dde491ab6bb704fc
lennysgarage <lennysgarage1@gmail.com>
2021-01-13 22:55:16 -0500

    Added a simple menu system to convert from dec to IEEE and vice-versa

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Scanner;
+
 public class Main extends Application {
 
     @Override
@@ -14,10 +16,45 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+        boolean running = true;
+
+        while(running) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("=========IEEE Converter=========");
+            System.out.println("2. IEEE-754 to Decimal");
+            System.out.println("1. Decimal to IEEE-754");
+            System.out.println("0. Exit");
+            System.out.println("=========IEEE Converter=========");
+
+            String input = in.nextLine();
+            switch (input) {
+                case "0":
+                    System.exit(0);
+                    break;
+                case "1":
+                    decToIEEE(Integer.parseInt(input));
+                    break;
+                case "2":
+                    IEEEToDec(input);
+                    break;
+                default:
+                    System.out.println("Plase enter a valid input (0-2)");
+                    break;
+            }
+        }
+    }
+
+    private static void decToIEEE(int dec){
+
+    }
+
+    private static void IEEEToDec(String ieee){
+
     }
 }
