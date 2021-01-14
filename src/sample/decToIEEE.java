443d8551d0a583a03ae63fd8a92a57544eb69259
Jackson <jackson5804@gmail.com>
2021-01-14 13:06:42 -0500

    Fixed up decToIEEE.java

--- src/sample/decToIEEE.java
+++ src/sample/decToIEEE.java
@@ -1,35 +1,42 @@
 package sample;
 
 public class decToIEEE {
+
     private double decimal;
-    private int intval;
-    private double mantissa;
+    private String binString;
 
     public decToIEEE(double decimal) {
         this.decimal = decimal;
-        intval = (int) this.decimal;
-        mantissa = this.decimal - this.intval;
+        this.binString = Integer.toBinaryString((int)this.decimal);
     }
 
-    public static String getIEEE(double intval) {
-        char signBit;
-        if (intval >= 0) {
-            signBit = '0';
-        } else {
-            signBit = '1';
+    public void getIEEE() {
+        System.out.println(getSignBit() + getBinary() + getMantissa());
+    }
+
+    private char getSignBit() {
+        if (this.decimal >= 0) {
+            return '0';
         }
-        String lmao = Integer.toBinaryString((int)intval);
-        int exponent = lmao.length() - 1;
-        String mantissa = lmao.substring(1);
+        return '1';
+    }
+
+    private String getBinary() {
+        int exponent = this.binString.length() - 1;
+        return Integer.toBinaryString(127 + exponent);
+    }
+
+    private String getMantissa() {
+        String mantissa = this.binString.substring(1);
         while (mantissa.length() < 23) {
-            intval = ((intval - (int)intval) * 2);
-            mantissa += String.valueOf((int) intval);
+            this.decimal = ((this.decimal - (int)this.decimal) * 2);
+            mantissa += String.valueOf((int) this.decimal);
         }
-        intval = ((intval - (int)intval) * 2);
-        if ((int) intval == 1) {
-            intval = ((intval - (int)intval) * 2);
-            String roundVal = String.valueOf(intval);
-            roundVal += String.valueOf((int)((intval - (int)intval) * 2));
+        this.decimal = ((this.decimal - (int)this.decimal) * 2);
+        if ((int) this.decimal == 1) {
+            this.decimal = ((this.decimal - (int)this.decimal) * 2);
+            String roundVal = String.valueOf(this.decimal);
+            roundVal += String.valueOf((int)((this.decimal - (int)this.decimal) * 2));
             if (roundVal.indexOf('1') != -1 || (int)(mantissa.charAt(mantissa.length() - 1)) == 0) {
                 mantissa = Integer.toBinaryString(Integer.parseInt(mantissa, 2) + 1);
             }
@@ -40,9 +47,7 @@ public class decToIEEE {
                 fill += "0";
             }
         }
-        String binary = Integer.toBinaryString(127 + exponent);
-        return signBit + binary + fill + mantissa;
+        return fill + mantissa;
     }
-
 
 }
