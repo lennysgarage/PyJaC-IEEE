d1fbe581004f2ea4fb787cefbc92ae1fc7ed4833
Jackson <jackson5804@gmail.com>
2021-01-14 16:31:23 -0500

    FINALLY

--- src/sample/decToIEEE.java
+++ src/sample/decToIEEE.java
@@ -4,9 +4,11 @@ public class decToIEEE {
 
     private double decimal;
     private String binString;
+    private int negativeExp;
 
     public decToIEEE(double decimal) {
         this.decimal = decimal;
+        this.negativeExp = 0;
         if (this.decimal < 0) {
             this.binString = Integer.toBinaryString(-(int) this.decimal);
         } else {
@@ -15,7 +17,10 @@ public class decToIEEE {
     }
 
     public void getIEEE() {
-        System.out.println(getSignBit() + getBinary() + getMantissa());
+        char signBit = getSignBit();
+        String mantissa = getMantissa();
+        String binary = getBinary();
+        System.out.println(signBit + binary + mantissa);
     }
 
     private char getSignBit() {
@@ -26,24 +31,45 @@ public class decToIEEE {
     }
 
     private String getBinary() {
-        int exponent = this.binString.length() - 1;
-        return Integer.toBinaryString(127 + exponent);
+        int exponent;
+        if (this.decimal == 0) {
+            exponent = -127;
+        } else if ((int)this.decimal == 0) {
+            exponent = this.negativeExp;
+        } else {
+            exponent = this.binString.length() - 1;
+        }
+        String binary = Integer.toBinaryString(127 + exponent);
+        while (binary.length() < 8) {
+            binary = "0" + binary;
+        }
+        return binary;
     }
 
     private String getMantissa() {
-        if (this.decimal < 0) {
-            this.decimal = -this.decimal;
+        boolean lessThanOne = (int) this.decimal == 0 && this.decimal != 0;
+        double decimal = this.decimal;
+        if (decimal < 0) {
+            decimal = -decimal;
         }
         String mantissa = this.binString.substring(1);
         while (mantissa.length() < 23) {
-            this.decimal = ((this.decimal - (int)this.decimal) * 2);
-            mantissa += String.valueOf((int) this.decimal);
+            decimal = ((decimal - (int)decimal) * 2);
+            if (lessThanOne && (int) decimal == 1) {
+                lessThanOne = false;
+                this.negativeExp--;
+            }
+            else if (!lessThanOne) {
+                mantissa += String.valueOf((int) decimal);
+            } else {
+                this.negativeExp--;
+            }
         }
-        this.decimal = ((this.decimal - (int)this.decimal) * 2);
-        if ((int) this.decimal == 1) {
-            this.decimal = ((this.decimal - (int)this.decimal) * 2);
-            String roundVal = String.valueOf(this.decimal);
-            roundVal += String.valueOf((int)((this.decimal - (int)this.decimal) * 2));
+        decimal = ((decimal - (int)decimal) * 2);
+        if ((int) decimal == 1) {
+            decimal = ((decimal - (int)decimal) * 2);
+            String roundVal = String.valueOf(decimal);
+            roundVal += String.valueOf((int)((decimal - (int)decimal) * 2));
             if (roundVal.indexOf('1') != -1 || (int)(mantissa.charAt(mantissa.length() - 1)) == 0) {
                 mantissa = Integer.toBinaryString(Integer.parseInt(mantissa, 2) + 1);
             }
